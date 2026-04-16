09acd70a1f4a5cd5ea6da213be645e7d1a580901
Rafael Santos <atresmarcas@gmail.com>
2026-04-15 23:51:25 -0300

    Desativando sitemao para finalizar deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://a3pi.netlify.app',
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx()], // Removido o sitemap daqui
     output: 'static'
 });
